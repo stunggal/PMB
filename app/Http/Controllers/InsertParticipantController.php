ccbf026ffdeb498c47e4ad24b3ca750b8cb78414
stunggal <54833890+stunggal@users.noreply.github.com>
2022-09-14 11:15:57 +0700

    Update InsertParticipantController.php

--- app/Http/Controllers/InsertParticipantController.php
+++ app/Http/Controllers/InsertParticipantController.php
@@ -13,7 +13,7 @@ class InsertParticipantController extends Controller
      */
     public function index()
     {
-        return view('insertparticipant.index');
+        return view('insertParticipant.index');
     }
 
     /**
